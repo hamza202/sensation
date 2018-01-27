cfa0c8525e4a82363773187ff68243fc2ee0a2b2
hamza202 <k8668975@gmail.com>
2018-01-27 15:50:47 +0200

    update 1

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// (function scrollReveal() {
+//     window.sr = ScrollReveal();
+//
+//     sr.reveal('.card-box', {
+//         duration   : 600,
+//         distance   : '20px',
+//         easing     : 'ease-out',
+//         origin     : 'bottom',
+//         reset      : true,
+//         scale      : 1,
+//         viewFactor : 0,
+//         afterReveal  : revealChildren
+//     }, 150);
+//
+//     var revealChildren = sr.reveal('.card-button, .card-text', {
+//         duration   : 500,
+//         scale      : 1,
+//         distance   : '20px',
+//         origin     : 'bottom',
+//         reset      : true,
+//         easing     : 'ease-out',
+//         viewFactor : 1
+//     }, 75);
+// })();
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.top-scroll', {easing: 'ease-out',scale: 1, origin: 'top', distance: '100px', duration: 1000 });
+sr.reveal('.bottom-scroll', {easing: 'ease-out',scale: 1, origin: 'bottom', distance: '50px', duration: 1000 });
+sr.reveal('.right-scroll', {origin: 'right', distance: '300px', duration: 2000});
+sr.reveal('.left-scroll', {origin: 'left', distance: '300px', duration: 2000});
